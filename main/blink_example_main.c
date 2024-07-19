98444aa467481170b93f85d71720c55ff7523acc
NguyenTan01 <nguyenbatan2502@gmail.com>
2024-07-19 08:14:19 -0700

    modifile main

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -96,7 +96,7 @@ void app_main(void)
 
     while (1) {
         ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        // blink_led();
+        blink_led();
         /* Toggle the LED state */
         s_led_state = !s_led_state;
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
